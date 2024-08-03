33acb5b896ff4e34a96e1c04c9f828f61d38929e
Muhammad <magaakhmadov@mail.ru>
2024-08-03 11:18:42 +0300

    MySite version2. Add templates and first urls

--- mySite/settings.py
+++ mySite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!uf%$nb5j6cj*p$^aui&35r@&!8&s#$p*-5w)gn!7m9dh=58jy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
